ee155e6c8d0e6526d9c21ef43ec1d35e86f0fa6d
Thomas Jakway <tjakway@nyu.com>
2015-11-02 18:59:04 -0500

    wrote blackjack function

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,12 +1,10 @@
-module Cards
-(
-Suit, CardValue, Card, newDeck, shuffleDeck
-) where
+module Cards where
 
 import Control.Applicative
 import System.Random
 import System.Random.Shuffle
 import Control.Monad.State
+import Data.List
 
 data Suit = Spade | Club | Heart | Diamond
                deriving (Eq, Ord, Enum, Bounded, Show, Read)
@@ -18,10 +16,19 @@ data Card = Card
   , cardValue :: CardValue
   } deriving (Show)
 
+--disambiguate between a player's hand and the deck--both are lists of
+--cards
+type Deck = [Card]
+type Hand = [Card]
+
+allSuits :: [Suit]
+allSuits = [minBound..maxBound] :: [Suit]
+
+allCardValues :: [CardValue]
+allCardValues = [minBound..maxBound] :: [CardValue]
+
 newDeck :: [Card]
 newDeck = Card <$> allSuits <*> allCardValues
-        where allSuits = [minBound..maxBound] :: [Suit]
-              allCardValues = [minBound..maxBound] :: [CardValue]
 
 shuffleDeck :: (RandomGen a) => (a, [Card]) -> [Card]
 shuffleDeck (gen, cards) = shuffle' cards (length cards) gen
@@ -29,3 +36,15 @@ shuffleDeck (gen, cards) = shuffle' cards (length cards) gen
 drawCard :: [Card] -> Maybe (Card, [Card])
 drawCard []     = Nothing
 drawCard (x:xs) = Just (x, xs)
+
+hasCard :: [Card] -> CardValue -> Bool
+hasCard cards whichCard = (elem True) . fmap ((==whichCard) . cardValue) $ cards
+
+blackjack :: Hand -> Bool
+blackjack hand = let hasAce = hasCard hand Ace 
+                     faceCards = Card <$> allSuits <*> [Jack, Queen, King]
+                     hasFaceCard = (elem True) . fmap (hasCard hand) . fmap (cardValue) $ faceCards
+                     in ((==2) . length $ hand) && (hasAce && hasFaceCard)
+
+handValue :: Hand -> Int
+handValue  = undefined
