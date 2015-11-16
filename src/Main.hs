15717a37cea754e634ce372f31ec30407838eb8f
Thomas Jakway <tjakway@nyu.com>
2015-11-16 14:49:10 -0500

    fixed typos in foldOverResult and gameResults in runNGamesNPlayers

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ import Cards
 -- |no need to create the empty list of starting records--it'll be
 -- generated automatically
 foldOverResults :: [ScoreRecord] -> [Result] -> [ScoreRecord]
-foldOverResults [] results = foldOverResults (flip . replicate mempty . length $ results) results 
+foldOverResults [] results = foldOverResults (flip replicate mempty . length $ results) results 
 foldOverResults startingRecords results = map (\(thisRecord, thisScore) -> thisRecord `addResult` thisScore) zippedArgs
                     where zippedArgs = zip startingRecords results
                           -- ^ TODO: come up with a better name...
@@ -14,7 +14,13 @@ foldOverResults startingRecords results = map (\(thisRecord, thisScore) -> thisR
 runNGamesNPlayers :: RandomGen a => Int -> Int -> a -> Maybe (ScoreRecord, [ScoreRecord])
 runNGamesNPlayers numGames numPlayers gen = let players = replicate numPlayers BasicPlayer 
                                                 dealer = BasicDealer
-                                                gameResults = map (playGame dealer players (infiniteShuffledDeck gen)) [1..numGames]
+                                                -- |There should be
+                                                -- a better way to do
+                                                -- this--mapping over
+                                                -- a list of ints that are
+                                                -- ignored is basically
+                                                -- just a loop
+                                                gameResults = map (\_ -> playGame dealer players (infiniteShuffledDeck gen)) [1..numGames]
                                                 -- |tally the scores
                                                 --http://stackoverflow.com/questions/9448570/using-map-with-two-lists-rather-than-one-can-you-nest
                                                 scores = foldr (\(tallyDealerScore, tallyPlayerScores) res -> res >>= \(prevDealerScore, prevPlayerScores) -> return (tallyDealerScore `mconcat` prevDealerScore, foldOverResults prevPlayerScores))
