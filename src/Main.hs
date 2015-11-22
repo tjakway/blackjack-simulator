ea2857ad9a52f238e11bf2e6e650f2c8a652daff
Thomas Jakway <tjakway@nyu.edu>
2015-11-22 13:11:29 -0500

    removing WIP code from master (finish in IO branch, then merge)

--- src/Main.hs
+++ src/Main.hs
@@ -7,25 +7,5 @@ import Jakway.Blackjack.Cards
 import Jakway.Blackjack.Result
 import Jakway.Blackjack.Visibility
 
--- |no need to create the empty list of starting records--it'll be
--- generated automatically
-foldOverResults :: [ScoreRecord] -> [Result] -> [ScoreRecord]
-foldOverResults [] results = foldOverResults (flip . replicate mempty . length $ results) results 
-foldOverResults startingRecords results = map (\(thisRecord, thisScore) -> thisRecord `addResult` thisScore) zippedArgs
-                    where zippedArgs = zip startingRecords results
-                          -- ^ TODO: come up with a better name...
-
-runNGamesNPlayers :: RandomGen a => Int -> Int -> a -> Maybe (ScoreRecord, [ScoreRecord])
-runNGamesNPlayers numGames numPlayers gen = let players = replicate numPlayers BasicPlayer 
-                                                dealer = BasicDealer
-                                                gameResults = map (playGame dealer players (infiniteShuffledDeck gen)) [1..numGames]
-                                                -- |tally the scores
-                                                --http://stackoverflow.com/questions/9448570/using-map-with-two-lists-rather-than-one-can-you-nest
-                                                scores = foldr (\(tallyDealerScore, tallyPlayerScores) res -> res >>= \(prevDealerScore, prevPlayerScores) -> return (tallyDealerScore `mconcat` prevDealerScore, foldOverResults prevPlayerScores))
-                                                startingValues = (mempty, replicate numPlayers mempty)
-                                                    in scores gameResults startingValues
-
-
-
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
