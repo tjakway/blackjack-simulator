3c2e20e42d7c9ae1a5378cd2cb015dcf83cc3ce7
Thomas Jakway <tjakway@nyu.edu>
2015-11-23 16:47:59 -0500

    wrote first statement function

--- src/Jakway/Blackjack/IO/Database.hs
+++ src/Jakway/Blackjack/IO/Database.hs
@@ -8,11 +8,15 @@ enableForeignKeys conn = run conn "PRAGMA foreign_keys = ON;" []
 flipInner2 :: (a -> b -> c -> d) -> a -> c -> b -> d
 flipInner2 f x y z = f x z y
 
-createDB :: IConnection a => a -> IO [Integer]
-createDB conn =
+createTables :: IConnection a => a -> IO [Integer]
+createTables conn =
             sequence $ map (flipInner2 run conn []) createTableStatements
         where createTableStatements = [ "CREATE TABLE cards (id INTEGER PRIMARY KEY AUTOINCREMENT, cardValue INTEGER NOT NULL, suit INTEGER NOT NULL)",
                                         "CREATE TABLE players (whichPlayer INTEGER PRIMARY KEY)",
                                         "CREATE TABLE hands (id INTEGER PRIMARY KEY AUTOINCREMENT, whichPlayer INTEGER, whichHand INTEGER, thisCard INTEGER, "
                                                             ++ "FOREIGN KEY(whichPlayer) REFERENCES players(whichPlayer), FOREIGN KEY(thisCard) REFERENCES cards(id) )",
                                         "CREATE TABLE matches (id INTEGER PRIMARY KEY AUTOINCREMENT, whichGame INTEGER, dealersHand INTEGER, thisPlayersHand INTEGER, playerResult INTEGER)" ]
+
+insertCardStatement :: (IConnection a) => a -> IO (Statement)
+--ignore the id field
+insertCardStatement conn = prepare conn "INSERT INTO cards(cardValue, suit) VALUES(?, ?)"
