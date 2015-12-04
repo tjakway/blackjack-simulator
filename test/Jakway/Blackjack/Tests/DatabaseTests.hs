9046f616f80accea3fc5b1375d036c9db0922998
Thomas Jakway <tjakway@nyu.edu>
2015-12-03 21:37:11 -0500

    forgot to initialize the database

--- test/Jakway/Blackjack/Tests/DatabaseTests.hs
+++ test/Jakway/Blackjack/Tests/DatabaseTests.hs
@@ -9,8 +9,9 @@ import Database.HDBC
 import Database.HDBC.Sqlite3
 import Database.HDBC.Session (withConnectionIO)
 import Test.HUnit
-import Control.Monad (liftM)
+import Control.Monad (liftM, when)
 import qualified Jakway.Blackjack.IO.Database as DB
+import Data.List (sort)
 
 test_db_name = "tmp_test.db"
 
@@ -32,4 +33,9 @@ testOpenDatabase = TestCase $ withTestDatabase $ (\_ -> do
                                     else assertFailure message)
                     where message = "Database "++test_db_name++" does not exist!"
 
-tests = TestList [TestLabel "testOpenDatabase" testOpenDatabase]
+testTableList = TestCase $ withTestDatabase $ \conn -> DB.initializeDatabase conn >> commit conn >> getTables conn >>= (\tables -> when (not $ tablesEqual tables) (assertFailure $ message tables))
+                where tables = ["cards", "players", "hands", "matches"]
+                      tablesEqual readTables = (sort tables) == (sort readTables)
+                      message readTables = "Database tables don't match!  Read tables: " ++ (show readTables)
+
+tests = TestList [TestLabel "testOpenDatabase" testOpenDatabase, TestLabel "testTableList" testTableList]
