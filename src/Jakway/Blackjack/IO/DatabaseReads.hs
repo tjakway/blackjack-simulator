931421e762edc46093ba54aa4bacc42aa1cb9eb7
Thomas Jakway <tjakway@nyu.edu>
2015-12-05 16:50:55 -0500

    wrote readPlayers

--- src/Jakway/Blackjack/IO/DatabaseReads.hs
+++ src/Jakway/Blackjack/IO/DatabaseReads.hs
@@ -1,3 +1,15 @@
 module Jakway.Blackjack.IO.DatabaseReads where
 
+import Jakway.Blackjack.Visibility
+import Jakway.Blackjack.Cards
+import Jakway.Blackjack.CardOps
+import Database.HDBC
+import qualified Data.Map.Strict as HashMap
+import Control.Monad (join)
 
+readPlayers :: (IConnection a) => a -> IO ([Int])
+readPlayers conn = do
+        values <- quickQuery' conn "SELECT whichPlayer FROM players" []
+        -- |quickQuery' returns [[SqlValue]].  Collapse the list.
+        let playerInts = map fromSql (join values)
+        return playerInts
