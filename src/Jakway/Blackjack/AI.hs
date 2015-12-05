7c56ed36355e9941c85d11a8f91b0fe0ba93c54b
Thomas Jakway <tjakway@nyu.edu>
2015-12-05 17:01:25 -0500

    deleted AI typeclass, created AI data class and made play a top-level function

--- src/Jakway/Blackjack/AI.hs
+++ src/Jakway/Blackjack/AI.hs
@@ -6,23 +6,17 @@ import Jakway.Blackjack.CardOps
 import Jakway.Blackjack.Points
 import Control.Monad.State
 
-class AI a where
-  play :: a -> Hand -> Deck -> (Hand, Deck)
+data AI = BasicDealer | BasicPlayer
 
-data BasicDealer = BasicDealer
-data BasicPlayer = BasicPlayer
-
-instance AI BasicDealer where
-  play BasicDealer myHand deck = flip runState deck $ do
-      let points = handPoints (map unwrapVisibility myHand)
-      if points < 17
-         then do
-           drawnCard <- drawCard
-           deck' <- get
-           return . fst $ play BasicDealer (Shown drawnCard : myHand) deck'
-         else 
-           return myHand
-
-
-instance AI BasicPlayer where
-  play BasicPlayer = play BasicDealer
+play :: AI -> Hand -> Deck -> (Hand, Deck)
+play BasicDealer myHand deck = flip runState deck $ do
+    let points = handPoints (map unwrapVisibility myHand)
+    if points < 17
+        then do
+        drawnCard <- drawCard
+        deck' <- get
+        return . fst $ play BasicDealer (Shown drawnCard : myHand) deck'
+        else 
+        return myHand
+-- |currently all players play the same
+play BasicPlayer myHand deck = play BasicDealer myHand deck
