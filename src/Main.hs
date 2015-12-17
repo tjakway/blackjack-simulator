8b2dbb46c9e99c4a306c5bab6326525ef55b3a2a
Thomas Jakway <tjakway@nyu.edu>
2015-12-16 19:21:01 -0500

    removed playNGamesNPlayers

--- src/Main.hs
+++ src/Main.hs
@@ -11,19 +11,5 @@ import Jakway.Blackjack.Visibility
 import Jakway.Blackjack.IO.DatabaseWrites
 import Jakway.Blackjack.IO.DatabaseReads
 
-{-
-playNGamesNPlayers :: RandomGen a => Int -> Int -> a -> Maybe (ScoreRecord, [ScoreRecord])
-playNGamesNPlayers numGames numPlayers gen = let players = replicate numPlayers BasicPlayer 
-                                                 dealer = BasicDealer
-                                                 -- |XXX: There should be
-                                                 -- a better way to do
-                                                 -- this--mapping over
-                                                 -- a list of ints that are
-                                                 -- ignored is basically
-                                                 -- just a loop
-                                                 gameResults = map (\_ -> playGame dealer players (infiniteShuffledDeck gen)) [1..numGames]
-                                                 startingValues = (mempty, replicate numPlayers mempty) :: (ScoreRecord, [ScoreRecord]) 
-                                                    in sumScores startingValues gameResults
--}
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
