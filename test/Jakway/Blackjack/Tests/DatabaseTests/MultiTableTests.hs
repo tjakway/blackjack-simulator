8f9aea08526ca91849409ce4af218f18fa1d614c
Thomas Jakway <tjakway@nyu.edu>
2016-01-14 08:52:40 -0800

    set up skeleton test

--- test/Jakway/Blackjack/Tests/DatabaseTests/MultiTableTests.hs
+++ test/Jakway/Blackjack/Tests/DatabaseTests/MultiTableTests.hs
@@ -1,4 +1,10 @@
 module Jakway.Blackjack.Tests.DatabaseTests.MultiTableTests (tests) where
 
+import Test.HUnit
+import Test.Framework
+import Test.Framework.Providers.HUnit
 
-tests = undefined
+createMultiTableTests :: Assertion
+createMultiTableTests = return ()
+
+tests = testGroup "MultiTableTests" [testCase "createMultiTableTests" createMultiTableTests]
