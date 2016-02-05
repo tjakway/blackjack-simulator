18244ffdf6e8dbeeba9d5d1edf7b024a71da288d
Thomas Jakway <tjakway@nyu.edu>
2016-02-05 18:24:27 -0500

    added type signatures with existentially quantified types extension

--- src/Jakway/Blackjack/Util.hs
+++ src/Jakway/Blackjack/Util.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module Jakway.Blackjack.Util where
 
+innerMapTuple4 :: forall t t1. (t -> t1) -> (t, t, t, t) -> (t1, t1, t1, t1)
 innerMapTuple4 f (a,b,c,d) = (f a, f b, f c, f d)
 
+innerMapTuple3 :: forall t t1. (t -> t1) -> (t, t, t) -> (t1, t1, t1)
 innerMapTuple3 f (a,b,c) = (f a, f b, f c)
