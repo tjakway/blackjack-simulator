0a56e603db3810003a1e4a9d76d0b192602c4d9e
Thomas Jakway <tjakway@nyu.edu>
2016-02-05 14:25:09 -0500

    integrated GameTests into the test suite

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -2,11 +2,12 @@ module Main where
 
 import qualified Jakway.Blackjack.Tests.DatabaseTests.BasicTests as BasicTests
 import qualified Jakway.Blackjack.Tests.DatabaseTests.MultiTableTests as MultiTableTests
+import qualified Jakway.Blackjack.Tests.GameTests as GameTests
 import Test.HUnit
 import Test.Framework
 import Test.Framework.Providers.HUnit
 
 main :: IO ()
 main = defaultMainWithOpts 
-            [BasicTests.tests, MultiTableTests.tests]
+            [BasicTests.tests, MultiTableTests.tests, GameTests.tests]
             mempty
