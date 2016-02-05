d3ee129ce3f619439bfa59f6e483e7b6e269aba0
Thomas Jakway <tjakway@nyu.edu>
2016-02-05 15:50:54 -0500

    began writing an integration test

---
+++ test/Jakway/Blackjack/Tests/IntegrationTests/MatchTests.hs
@@ -0,0 +1,24 @@
+module Jakway.Blackjack.Tests.IntegrationTests (tests) where
+
+import Jakway.Blackjack.Tests.GameTests (test_1v1_game)
+import Jakway.Blackjack.IO.DatabaseCommon
+import Jakway.Blackjack.IO.DatabaseWrites
+import Jakway.Blackjack.IO.DatabaseReads
+import Test.HUnit
+import Test.Framework
+import Test.Framework.Providers.HUnit
+
+--run a very simple match, write it to the database, and read it back
+testReadWrite1v1 :: Assertion
+testReadWrite1v1 = withSingleTableTestDatabase $ \conn -> do
+        let (origDealersHand, origPlayersHand, origRes) = test_1v1_game
+        --there's only one player and player 0 is the dealer
+        let playerID = [1]
+
+        insertPlayers conn basicTestTableNames 2
+        insMatchStatement <- insertMatchStatement conn
+        insHandStatement <- insertHandStatement conn
+        insertMatch insMatchStatement insHandStatement conn basicTestTableNames origDealersHand (origPlayersHand, playerID, origRes)
+
+
+tests = testGroup "IntegrationTests" [testCase "testReadWrite1v1" testReadWrite1v1]
