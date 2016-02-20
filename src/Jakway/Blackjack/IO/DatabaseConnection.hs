dae3b1f0466cbf5958e00526b5bc7f7d84982d0b
Thomas Jakway <tjakway@nyu.edu>
2016-02-20 17:50:59 -0500

    mayReadFile now works--checks if file exists before opening now depending on System.Directory for doesFileExist;

--- src/Jakway/Blackjack/IO/DatabaseConnection.hs
+++ src/Jakway/Blackjack/IO/DatabaseConnection.hs
@@ -15,6 +15,7 @@ import Control.Exception
 
 #ifdef BUILD_POSTGRESQL
 import Database.HDBC.PostgreSQL
+import System.Directory (doesFileExist)
 
 postgres_conf_filename :: FilePath
 postgres_conf_filename = "psql_conn.conf"
@@ -24,12 +25,15 @@ default_connection_string = ""
 
 mayReadFile :: FilePath -> IO (Maybe String)
 mayReadFile path = do
-    result <- (try (evaluate (readFile path))) :: IO (Either IOException (IO String))
-    case result of
-        Left _ -> return Nothing
-        --contents has a type of IO String, need to convert it to an 
-        --IO (Maybe String)
-        Right contents -> contents >>= \c -> return . Just $ c
+    exists <- doesFileExist path
+    if exists == False then return Nothing
+                       else do
+        result <- (try (evaluate (readFile path))) :: IO (Either IOException (IO String))
+        case result of
+            Left _ -> return Nothing
+            --contents has a type of IO String, need to convert it to an 
+            --IO (Maybe String)
+            Right contents -> contents >>= \c -> return . Just $ c
 
 readPostgresConnectionString :: IO (String)
 readPostgresConnectionString = do
