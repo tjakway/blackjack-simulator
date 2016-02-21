946443ef5799fd141385191aa8f1af035737d1ec
Thomas Jakway <tjakway@nyu.edu>
2016-02-20 20:09:48 -0500

    syntax

--- src/Jakway/Blackjack/IO/RDBMS/Postgres.hs
+++ src/Jakway/Blackjack/IO/RDBMS/Postgres.hs
@@ -40,4 +40,3 @@ createTables conn tableNames =
                                           createHandsTable,
                                           createMatchesTable,
                                           commitTransac ]
-
