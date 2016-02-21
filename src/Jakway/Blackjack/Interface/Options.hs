dd8e857b4fe2212eeb1a8d69ce0363c1ed305407
Thomas Jakway <tjakway@nyu.edu>
2016-02-21 15:57:15 -0500

    writing options processing

--- src/Jakway/Blackjack/Interface/Options.hs
+++ src/Jakway/Blackjack/Interface/Options.hs
@@ -1,13 +1,73 @@
-module Jakway.Blackjack.Interface.Options where
+module Jakway.Blackjack.Interface.Options
+(
+Flag(..),
+processOptions
+) where
 
-import qualified System.Console.GetOpt.Simple as Opt
+import Jakway.Blackjack.AI
+import System.Console.GetOpt
+import Data.Maybe (fromJust, catMaybes)
 
-options = 
+data Flag = Verbose |
+            WhichDealer AI |
+            NumBasicPlayer Int |
+            NumGames Integer
+            deriving Show
 
-let options = makeOptions [ (noArg, "verbose", Optional,   "Be verbose."),
-                            (noArg,   "with-BasicDealer",    Optional,   "Use BasicDealer AI."),
-                            (arg,   "num-BasicPlayer",    Optional,   "Number of BasicPlayer AI's."),
-                            (arg,   "num-games", Required, "Number of games to run.")
-                           ]
+type Config = (Bool, AI, [AI], Integer)
 
-(opts, args) <- getOptsArgs (makeOptions options) ["conf", "section"] ["command"]
+--extract the number of BasicPlayer or return Nothing
+getNumBasicPlayer :: Flag -> Maybe Int
+getNumBasicPlayer (NumBasicPlayer num) = Just num
+getNumBasicPlayer _ = Nothing
+
+getWhichDealer :: Flag -> Maybe AI
+getWhichDealer (WhichDealer whichAI) = Just whichAI
+getWhichDealer _ = Nothing
+
+getNumGames :: Flag -> Maybe Integer
+getNumGames (NumGames num) = Just num
+getNumGames _ = Nothing
+
+flagsToConfig :: [Flag] -> Either String Config
+flagsToConfig [] = Left "No flags passed."
+flagsToConfig flags
+                --No dealer!
+                | (whichDealer == Nothing) = Left "No dealer AI found, perhaps you passed a player AI by mistake?"
+                | (numPlayerAIs == 0) = Left "Must have >0 players!"
+                | otherwise = Right (hasVerbose, (fromJust whichDealer), playerAIs, numGames)
+            --Make sure a player AI hasn't been passed for a dealer AI
+            -- TODO: rewrite using filter?
+            where whichDealer = case (catMaybes $ map getWhichDealer flags) of [] -> Nothing
+                                                                               [x] -> Just x
+                  getNum f = length . catMaybes $ map f flags
+                  --need at least 1 player besides the dealer
+                  numBasicPlayerAIs = getNum getNumBasicPlayer
+                  numGames = getNum getNumGames
+                  hasVerbose = (map (== Verbose) flags) `elem` True
+                  --build the list of player AIs from the passed flags
+                  playerAIs = replicate numBasicPlayerAIs BasicPlayer
+                  
+
+--flag converters
+dealerOpt, ngOpt, nbpOpt :: String -> Flag
+dealerOpt = WhichDealer . read
+nbpOpt = NumBasicPlayer . read
+ngOpt = NumGames . read
+
+
+options :: [OptDescr Flag]
+options =
+    [ Option ['v']     ["verbose"] (NoArg Verbose)       "Be verbose."
+    , Option []        ["with-dealer"]  (ReqArg dealerOpt "DealerAI")  "Which dealer AI to use."
+    , Option []        ["num-BasicPlayer"]  (ReqArg nbpOpt  "NUM")  "Number of BasicPlayer AI's."
+    , Option ['g']     ["num-games"]  (ReqArg ngOpt "NUM") "Number of games to run."
+    ]
+
+
+processOptions :: [String] -> IO ([Flag], [String])
+processOptions argv = 
+    case getOpt Permute options argv of
+        (o,n,[]  ) -> return (o,n)
+        (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    where header = "Usage: blackjack-simulator [OPTION...]"
