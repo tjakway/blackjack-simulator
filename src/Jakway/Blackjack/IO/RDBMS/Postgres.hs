439d22d8a227c22b7b202b2400abcedc50c0ef78
Thomas Jakway <tjakway@nyu.edu>
2016-02-20 20:07:06 -0500

    rewrote createTables using ssub

--- src/Jakway/Blackjack/IO/RDBMS/Postgres.hs
+++ src/Jakway/Blackjack/IO/RDBMS/Postgres.hs
@@ -7,33 +7,37 @@ import Jakway.Blackjack.Util
 import Jakway.Blackjack.IO.TableNames
 import Database.HDBC
 
+
+-- ****IMPORTANT****
+-- SQL doesn't allow dynamic table
+-- names so they have to be manually
+-- inserted
+-- this is technically a SQL
+-- injection vulnerability but it's
+-- not like we have any malicious
+-- users...
+
+
 --Postgres uses SERIAL instead of SERIAL
 createTables :: IConnection a => a -> TableNames -> IO ()
 createTables conn tableNames =
             mapM_ (flipInner2 run conn []) createTableStatements
-        where createTableStatements = [ "BEGIN TRANSACTION",
-                                      "CREATE TABLE IF NOT EXISTS cards (id SERIAL PRIMARY KEY, cardValue INTEGER NOT NULL, suit INTEGER NOT NULL, visible INTEGER NOT NULL)",
-                                      -- ^ Sqlite doesn't have a boolean
-                                      -- data type, see https://www.sqlite.org/datatype3.html and http://stackoverflow.com/questions/843780/store-boolean-value-in-sqlite
-                                      -- ****IMPORTANT****
-                                      -- SQL doesn't allow dynamic table
-                                      -- names so they have to be manually
-                                      -- inserted
-                                      -- this is technically a SQL
-                                      -- injection vulnerability but it's
-                                      -- not like we have any malicious
-                                      -- users...
-                                        "CREATE TABLE IF NOT EXISTS "++ (getPlayerTableName tableNames) ++" (whichPlayer INTEGER PRIMARY KEY)",
-                                        "COMMIT TRANSACTION",
-                                        "BEGIN TRANSACTION",
-                                        -- | Note that SQLite doesn't allow
-                                        -- any datatype other than INTEGER
-                                        -- to be declared PRIMARY KEY
-                                        -- SERIAL
-                                        "CREATE TABLE IF NOT EXISTS "++ (getHandTableName tableNames)  ++
-                                            "(id SERIAL PRIMARY KEY, whichPlayer INTEGER REFERENCES "++ (getPlayerTableName tableNames) ++ ", whichHand BIGINT REFERENCES "++ (getHandTableName tableNames)++", thisCard INTEGER REFERENCES cards, ",
+        where   ptn = getPlayerTableName tableNames  --"player table name"
+                htn = getHandTableName tableNames
+                mtn = getMatchTableName tableNames
+                beginTransac = "BEGIN TRANSACTION"
+                commitTransac = "COMMIT TRANSACTION"
+                createCardTable = "CREATE TABLE IF NOT EXISTS cards (id SERIAL PRIMARY KEY, cardValue INTEGER NOT NULL, suit INTEGER NOT NULL, visible INTEGER NOT NULL)"
+                createPlayersTable = ssub "CREATE TABLE IF NOT EXISTS ?(whichPlayer INTEGER PRIMARY KEY)" [ptn]
+                createHandsTable = ssub "CREATE TABLE IF NOT EXISTS ?(id SERIAL PRIMARY KEY, whichPlayer INTEGER REFERENCES ?, whichHand BIGINT, thisCard INTEGER REFERENCES cards)" [htn, ptn ++ "(whichPlayer)"]
+                createMatchesTable = ssub "CREATE TABLE IF NOT EXISTS ?(id SERIAL PRIMARY KEY, whichGame INTEGER REFERENCES, dealersHand BIGINT REFERENCES ?, whichPlayer INTEGER REFERENCES ?, thisPlayersHand BIGINT REFERENCES ?, playerResult INTEGER)" [mtn, htn ++ "(whichHand)", ptn ++ "(whichPlayer)"]
+                createTableStatements = [ beginTransac,
+                                          createCardTable,
+                                          createPlayersTable,
+                                          commitTransac,
 
-                                        "CREATE TABLE IF NOT EXISTS "++ (getMatchTableName tableNames) ++
-                                            "(id SERIAL PRIMARY KEY, whichGame INTEGER REFERENCES, dealersHand BIGINT REFERENCES "++ (getHandTableName tableNames) ++", whichPlayer INTEGER REFERENCES "++ (getPlayerTableName tableNames) ++ ", thisPlayersHand BIGINT REFERENCES " ++ (getHandTableName tableNames) ++ ", playerResult INTEGER, " ++
-                                                              "FOREIGN KEY(dealersHand) REFERENCES "++ (getHandTableName tableNames) ++"(whichHand), FOREIGN KEY(whichPlayer) REFERENCES "++ (getPlayerTableName tableNames) ++"(whichPlayer), FOREIGN KEY(thisPlayersHand) REFERENCES "++ (getHandTableName tableNames) ++"(whichHand) ) ",
-                                                             "COMMIT TRANSACTION" ]
+                                          beginTransac,
+                                          createHandsTable,
+                                          createMatchesTable,
+                                          commitTransac ]
+
