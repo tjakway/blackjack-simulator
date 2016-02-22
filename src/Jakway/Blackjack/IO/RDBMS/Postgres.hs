26882e05e60fe7d6cf5b0f5d01f63fffeca22dd2
Thomas Jakway <tjakway@nyu.edu>
2016-02-22 12:14:51 -0500

    changed serial -> bigserial

--- src/Jakway/Blackjack/IO/RDBMS/Postgres.hs
+++ src/Jakway/Blackjack/IO/RDBMS/Postgres.hs
@@ -18,7 +18,7 @@ import Database.HDBC
 -- users...
 
 
---Postgres uses SERIAL instead of SERIAL
+--Postgres uses SERIAL instead of AUTO INCREMENT
 createTables :: IConnection a => a -> TableNames -> IO ()
 createTables conn tableNames =
             mapM_ (flipInner2 run conn []) createTableStatements
@@ -29,8 +29,8 @@ createTables conn tableNames =
                 commitTransac = "COMMIT TRANSACTION"
                 createCardTable = "CREATE TABLE IF NOT EXISTS cards (id INTEGER PRIMARY KEY, cardValue INTEGER NOT NULL, suit INTEGER NOT NULL, visible INTEGER NOT NULL)"
                 createPlayersTable = ssub "CREATE TABLE IF NOT EXISTS ?(whichPlayer INTEGER PRIMARY KEY, aiType TEXT NOT NULL)" [ptn]
-                createHandsTable = ssub "CREATE TABLE IF NOT EXISTS ?(id SERIAL PRIMARY KEY, whichPlayer INTEGER REFERENCES ?, whichHand BIGINT NOT NULL, thisCard INTEGER REFERENCES cards(id))" [htn, ptn ++ "(whichPlayer)"]
-                createMatchesTable = ssub "CREATE TABLE IF NOT EXISTS ?(id SERIAL PRIMARY KEY, whichGame INTEGER NOT NULL, dealersHand BIGINT NOT NULL, whichPlayer INTEGER REFERENCES ?, thisPlayersHand BIGINT NOT NULL, playerResult INTEGER NOT NULL)" [mtn, ptn ++ "(whichPlayer)"]
+                createHandsTable = ssub "CREATE TABLE IF NOT EXISTS ?(id BIGSERIAL PRIMARY KEY, whichPlayer INTEGER REFERENCES ?, whichHand BIGINT NOT NULL, thisCard INTEGER REFERENCES cards(id))" [htn, ptn ++ "(whichPlayer)"]
+                createMatchesTable = ssub "CREATE TABLE IF NOT EXISTS ?(id BIGSERIAL PRIMARY KEY, whichGame INTEGER NOT NULL, dealersHand BIGINT NOT NULL, whichPlayer INTEGER REFERENCES ?, thisPlayersHand BIGINT NOT NULL, playerResult INTEGER NOT NULL)" [mtn, ptn ++ "(whichPlayer)"]
                 createTableStatements = [ beginTransac,
                                           createCardTable,
                                           createPlayersTable,
