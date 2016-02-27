5b1031097a049d8291f3ba75ed6bea58f9602ddf
Thomas Jakway <tjakway@nyu.edu>
2016-02-27 11:04:23 -0500

    fixed ProgMain module name

--- test/Jakway/Blackjack/Tests/IntegrationTests/MainTests.hs
+++ test/Jakway/Blackjack/Tests/IntegrationTests/MainTests.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE CPP #-}
 module Jakway.Blackjack.Tests.IntegrationTests.MainTests (testCases) where
 
-import qualified ProgMain
+import qualified Jakway.Blackjack.ProgMain as ProgMain
 import Test.HUnit hiding (State)
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import qualified System.Environment as Env
 import Jakway.Blackjack.IO.TableNames
+import Jakway.Blackjack.IO.DatabaseConnection
 import Database.HDBC
 
 -- TODO: implement for SQLite
@@ -19,7 +20,7 @@ connectDB = connectPostgresDBReadString
 testInsertOneMatch :: Assertion
 testInsertOneMatch = do
         let suffix = "test_ins_one_match_suff"
-        Env.withArgs ["-v", "--with-dealer=BasicDealer", "--num-BasicPlayer=1", "--num-games=10", "--tablename-suffix=" ++ suffix] ProgMain.main
+        Env.withArgs ["-v", "--with-dealer=BasicDealer", "--num-BasicPlayer=1", "--num-games=10", "--tablename-suffix=" ++ suffix] ProgMain.progMain
 
         conn <- connectDB
         assertTablesExist conn (getTableNames suffix)
