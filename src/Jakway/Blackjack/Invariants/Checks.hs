91bccfb34e6bfca1704c13ad87cc77967f0ca37f
Thomas Jakway <tjakway@nyu.edu>
2016-03-02 14:52:51 -0500

    wrote first basic check: a hand should only have 1 hidden card

--- src/Jakway/Blackjack/Invariants/Checks.hs
+++ src/Jakway/Blackjack/Invariants/Checks.hs
@@ -1,5 +1,11 @@
 module Jakway.Blackjack.Invariants.Checks where
 
+import Jakway.Blackjack.CardOps
+import Jakway.Blackjack.Visibility
 
 checkHand :: Hand -> Either String Bool
-checkHand = undefined
+checkHand hand = numHiddenCheck >>= (\b -> Right True)
+        where numHidden = foldr (\thisCard hiddenSum -> case thisCard
+                                of (Hidden _) -> hiddenSum + 1
+                                   (Shown _) -> hiddenSum) 0 hand
+              numHiddenCheck = if numHidden /= 1 then Left "Should only have 1 face down card" else Right True
