0bdd2d039140b2d213f0dbcfa840fef3165e1aaa
Thomas Jakway <tjakway@nyu.edu>
2016-03-06 21:29:43 -0500

    added testPlayerBust

--- test/Jakway/Blackjack/Tests/AITests/BasicPlayerTests.hs
+++ test/Jakway/Blackjack/Tests/AITests/BasicPlayerTests.hs
@@ -2,6 +2,8 @@ module Jakway.Blackjack.Tests.AITests.BasicPlayerTests (testCases) where
 
 import Jakway.Blackjack.Tests.Constants
 import Jakway.Blackjack.CardOps
+import Jakway.Blackjack.Cards
+import Jakway.Blackjack.Visibility
 import Jakway.Blackjack.AI
 import Jakway.Blackjack.Match
 import Jakway.Blackjack.Game
@@ -10,6 +12,7 @@ import Control.Monad.State (evalState)
 import Test.HUnit
 import Test.Framework
 import Test.Framework.Providers.HUnit
+import Jakway.Blackjack.Result
 
 sanityCheck :: Assertion
 sanityCheck = let resCards = (flip evalState) testDeck $ do
@@ -27,10 +30,15 @@ tie_1v1 = do
         assertBool message (length (head playerHand) == 3)
 
 
-testDealerWin :: Assertion
-testDealerWin = let match = evalGame BasicDealer [BasicPlayer] deck
-
+testPlayerBust :: Assertion
+testPlayerBust = let maybeMatch = evalGame BasicDealer [BasicPlayer] deck
+                     expectedDealerHand = [Hidden $ Card Club Four, Shown $ Card Heart Three, Shown $ Card Heart Eight, Shown $ Card Club Six]
+                     expectedPlayerHand = [Hidden $ Card Heart Six, Shown $ Card Diamond Seven, Shown $ Card Spade Nine]
+                     expectedResult = Lose
+                    in case maybeMatch of Nothing -> assertFailure "Failed to run match"
+                                          Just (Match dHand pIds pHands pRes) -> assertEqual "Dealer's hand didn't match" expectedDealerHand dHand >> 
+                                                                assertEqual "Player's hand didn't match" expectedPlayerHand (head pHands) >> assertEqual "Incorrect result" expectedResult (head pRes)
         where deck = [Card {cardSuit = Club, cardValue = Four},Card {cardSuit = Heart, cardValue = Three},Card {cardSuit = Heart, cardValue = Six},Card {cardSuit = Diamond, cardValue = Seven},Card {cardSuit = Spade, cardValue = Nine},Card {cardSuit = Heart, cardValue = Eight},Card {cardSuit = Club, cardValue = Six},Card {cardSuit = Spade, cardValue = Ace},Card {cardSuit = Heart, cardValue = Ten},Card {cardSuit = Club, cardValue = Ace},Card {cardSuit = Spade, cardValue = Four},Card {cardSuit = Club, cardValue = Three},Card {cardSuit = Diamond, cardValue = Eight},Card {cardSuit = Diamond, cardValue = Jack},Card {cardSuit = Spade, cardValue = Seven},Card {cardSuit = Diamond, cardValue = Three},Card {cardSuit = Diamond, cardValue = Ten},Card {cardSuit = Heart, cardValue = Two},Card {cardSuit = Heart, cardValue = Queen},Card {cardSuit = Spade, cardValue = Five},Card {cardSuit = Heart, cardValue = King},Card {cardSuit = Heart, cardValue = Five},Card {cardSuit = Club, cardValue = Eight},Card {cardSuit = Spade, cardValue = Eight},Card {cardSuit = Spade, cardValue = Six},Card {cardSuit = Diamond, cardValue = Six},Card {cardSuit = Diamond, cardValue = Queen},Card {cardSuit = Spade, cardValue = King},Card {cardSuit = Diamond, cardValue = Nine},Card {cardSuit = Club, cardValue = Nine},Card {cardSuit = Heart, cardValue = Nine},Card {cardSuit = Spade, cardValue = Two},Card {cardSuit = Diamond, cardValue = Two},Card {cardSuit = Diamond, cardValue = Four},Card {cardSuit = Spade, cardValue = Jack},Card {cardSuit = Club, cardValue = Five},Card {cardSuit = Heart, cardValue = Four},Card {cardSuit = Diamond, cardValue = King},Card {cardSuit = Club, cardValue = Ten},Card {cardSuit = Heart, cardValue = Jack},Card {cardSuit = Diamond, cardValue = Ace},Card {cardSuit = Club, cardValue = King},Card {cardSuit = Club, cardValue = Two},Card {cardSuit = Diamond, cardValue = Five},Card {cardSuit = Heart, cardValue = Seven},Card {cardSuit = Club, cardValue = Jack},Card {cardSuit = Spade, cardValue = Three},Card {cardSuit = Spade, cardValue = Queen},Card {cardSuit = Club, cardValue = Queen},Card {cardSuit = Spade, cardValue = Ten},Card {cardSuit = Club, cardValue = Seven},Card {cardSuit = Heart, cardValue = Ace}]
 
 
-testCases = [testCase "sanityCheck" sanityCheck, testCase "tie_1v1" tie_1v1]
+testCases = [testCase "sanityCheck" sanityCheck, testCase "tie_1v1" tie_1v1, testCase "testPlayerBust" testPlayerBust]
