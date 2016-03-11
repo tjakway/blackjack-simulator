9a3d0f0b0921c21bfe1ec672c7adbafafe143890
Thomas Jakway <tjakway@nyu.edu>
2016-03-11 12:16:09 -0500

    refacted handChecks and added list of invariants to implement

--- src/Jakway/Blackjack/Invariants/Checks.hs
+++ src/Jakway/Blackjack/Invariants/Checks.hs
@@ -4,11 +4,19 @@ import Jakway.Blackjack.CardOps
 import Jakway.Blackjack.Visibility
 
 checkHand :: Hand -> Either String Bool
-checkHand hand = numHiddenCheck >>= (\b -> Right True)
+checkHand hand = hiddenCardsCheck hand
+
+hiddenCardsCheck :: Hand -> Either String Bool
+hiddenCardsCheck hand = numHiddenCheck >>= (\b -> Right True)
+
         where numHidden = foldr (\thisCard hiddenSum -> case thisCard
                                 of (Hidden _) -> hiddenSum + 1
                                    (Shown _) -> hiddenSum) 0 hand
               numHiddenCheck = if numHidden /= 1 then Left "Should only have 1 face down card" else Right True
 
 --invariants to implement:
---  * only one hidden card per hand
+-- * if both players bust it should be a tie
+-- * no player should be over 30 (because it would mean they busted and
+-- then hit again, which should never happen)
+
+
