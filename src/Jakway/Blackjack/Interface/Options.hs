2f6d57e6eb419fed2c283596a36d4a80cbb11543
Thomas Jakway <tjakway@nyu.edu>
2016-03-29 07:26:28 -0400

    added FiftyFiftyPlayer to Options

--- src/Jakway/Blackjack/Interface/Options.hs
+++ src/Jakway/Blackjack/Interface/Options.hs
@@ -15,13 +15,29 @@ import qualified Jakway.Blackjack.Interface.Config as Conf
 data Flag = Verbose |
             WhichDealer AI |
             NumBasicPlayer Int |
+            NumFiftyFiftyPlayer Int |
             NumGames Integer |
             TableNameSuffix String |
             PostgresqlConnectString String
             deriving (Show, Eq)
 
 
---extract the number of BasicPlayer or return Nothing
+-- |returns the underlying value of the underlying flag if exactly one
+-- exists
+getSingleFlag :: (Flag -> Maybe a) -> [Flag] -> Maybe a
+getSingleFlag f allFlags = case catMaybes $ map f allFlags of [x] -> Just x
+                                                              [] -> Nothing
+
+-- |TODO: is there any way to eliminate the boilerplate from writing a lot
+-- of pattern matching functions?
+-- how do you write a generic "if the constructors match extract this flag"
+
+
+getNumFiftyFiftyPlayer :: Flag -> Maybe Int
+getNumFiftyFiftyPlayer (NumFiftyFiftyPlayer num) = Just num
+getNumFiftyFiftyPlayer _ = Nothing
+
+-- |extract the number of BasicPlayer or return Nothing
 getNumBasicPlayer :: Flag -> Maybe Int
 getNumBasicPlayer (NumBasicPlayer num) = Just num
 getNumBasicPlayer _ = Nothing
@@ -52,12 +68,15 @@ flagsToConfig flags
             -- TODO: rewrite using filter?
             where whichDealer = case (catMaybes $ map getWhichDealer flags) of [] -> Nothing
                                                                                [x] -> Just x
-                  getNum f = length . catMaybes $ map f flags
                   --need at least 1 player besides the dealer
                   numBasicPlayerAIs = let n = catMaybes $ map getNumBasicPlayer flags
                                           in case n of [] -> 0
                                                        _ -> head n
-                  numPlayerAIs = numBasicPlayerAIs -- TODO: modify as we add more AI types
+                  numFiftyFiftyPlayerAIs :: Int
+                  numFiftyFiftyPlayerAIs = case getSingleFlag getNumFiftyFiftyPlayer flags of (Just x) -> x
+                                                                                              Nothing -> 0
+                  -- | TODO: modify as we add more AI types
+                  numPlayerAIs = numBasicPlayerAIs + numFiftyFiftyPlayerAIs
                   suffixes = catMaybes $ map (\a -> case a of (TableNameSuffix suff) -> Just suff
                                                               _ -> Nothing) flags
                   numGames = case catMaybes (map getNumGames flags) of [] -> Nothing
@@ -65,7 +84,7 @@ flagsToConfig flags
                   hasVerbose = True `elem` (map (== Verbose) flags)
                   --build the list of player AIs from the passed flags
                   -- TODO: concat other ais as we add more types
-                  playerAIs = replicate numBasicPlayerAIs BasicPlayer
+                  playerAIs = (replicate numBasicPlayerAIs BasicPlayer) ++ (replicate numFiftyFiftyPlayerAIs FiftyFiftyPlayer)
                   tableNameSuffix = head suffixes
 
                   pConnStr :: Maybe String
@@ -78,9 +97,10 @@ extractFlags f flags = case (catMaybes $ map f flags) of [] -> Nothing
 
 
 --flag converters
-dealerOpt, ngOpt, nbpOpt :: String -> Flag
+dealerOpt, nbpOpt, nffpOpt, ngOpt:: String -> Flag
 dealerOpt = WhichDealer . read
 nbpOpt = NumBasicPlayer . read
+nffpOpt = NumFiftyFiftyPlayer . read
 ngOpt = NumGames . (read :: String -> Integer)
 
 options :: [OptDescr Flag]
@@ -88,7 +108,7 @@ options =
     [ Option ['v']     ["verbose"] (NoArg Verbose)       "Be verbose."
     , Option []        ["with-dealer"]  (ReqArg dealerOpt "DealerAI")  "Which dealer AI to use."
     , Option []        ["num-BasicPlayer"]  (ReqArg nbpOpt  "NUM")  "Number of BasicPlayer AI's."
-    , Option []        ["num-FiftyFiftyPlayer"]  (ReqArg nbpOpt  "NUM")  "Number of FiftyFiftyPlayer AI's."
+    , Option []        ["num-FiftyFiftyPlayer"]  (ReqArg nffpOpt  "NUM")  "Number of FiftyFiftyPlayer AI's."
     , Option ['g']     ["num-games"]  (ReqArg ngOpt "NUM") "Number of games to run."
     , Option ['s']     ["tablename-suffix"] (ReqArg TableNameSuffix "SUFFIX") "Table name suffix."
     , Option []        ["postgres-string"] (ReqArg PostgresqlConnectString "CONNSTR")  "Postgresql connection string (used instead of config file if both are present)"
