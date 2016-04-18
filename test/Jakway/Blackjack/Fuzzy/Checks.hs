f44cf2da63794e1d6705c9a922b31ddfcdad8883
Thomas Jakway <tjakway@nyu.edu>
2016-04-18 08:09:30 -0400

    resolved import errors

--- test/Jakway/Blackjack/Fuzzy/Checks.hs
+++ test/Jakway/Blackjack/Fuzzy/Checks.hs
@@ -2,48 +2,45 @@ module Jakway.Blackjack.Fuzzy.Checks where
 
 import qualified Statistics.Test.Types as Stats
 import qualified Statistics.Test.ChiSquared as Stats
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 import System.Random
-import Jakway.Blackjack.AI (deckToRNG)
+import Jakway.Blackjack.AI
 import Jakway.Blackjack.CardOps 
+import Jakway.Blackjack.Game
 import Data.Hashable
+import Control.Monad (liftM)
 
 deck_value_range = 104 -- ^ 52 cards plus the visibility flag
 
--- |get stdgen -> newshuffled deck -> stdgen -> integer 
-rngOutputWithRange :: (RandomGen g) => g -> (Int, (Int, Int))
-rngOutputWithRange gen = let deck = infiniteShuffledDeck gen
-                             rngFromDeck = deckToRNG deck
-                         in (fst . next $ rngFromDeck, genRange rngFromDeck)
 
 
--- |same as above but ignore the range
-rngOutput :: (RandomGen g) => g -> Int
-rngOutput gen = fst $ rngOutputWithRange gen
-
-
-
--- |record a new observation, updating the vector in place
-deckToObservation :: Vector Int -> Deck -> Vector Int
-deckToObservation vec deck = modify (\v -> write v index newCount)
+-- |record a new observation, updating the U.Vector in place
+deckToObservation :: U.Vector Int -> Deck -> U.Vector Int
+deckToObservation vec deck = U.modify (\v -> UM.write v index newCount)
         where index = (`mod` deck_value_range) . hash $ deck
-              newCount = (vec ! index) + 1
+              newCount = (vec U.! index) + 1
 
                      
-testDeckRandomness :: Double -> Integer -> AI -> [AI] -> IO TestResult
+testDeckRandomness :: Double -> Integer -> AI -> [AI] -> IO Stats.TestResult
 testDeckRandomness pvalue numSamples dealerAI playerAIs = do
         
-        let samplesVec = (fromList (replicate deck_value_range 0)) :: Vector Int
+        let samplesVec = (U.fromList (replicate deck_value_range 0)) :: U.Vector Int
 
-        observations <- foldM (\vec _ -> newDeckIO >>= (\d -> return $ evalGameKeepDeck dealerAI playerAIs d >>= \maybeDeck -> 
+        observations <- U.foldM' (\vec _ -> newDeckIO >>= (\d -> return $ evalGameKeepDeck dealerAI playerAIs d >>= \maybeDeck -> 
                                 case maybeDeck of Nothing -> return vec
-                                                (Just resDeck) -> return $ deckToObservation vec resDeck)) samplesVec [1..numSamples] 
+                                                  (Just resDeck) -> return $ deckToObservation vec resDeck)) samplesVec [1..numSamples] 
+
+        let additionalDF = (length observations) - 1
+
+        
+        return Stats.chi2test pvalue additionalDF (evenDistribution observations)
 
         where newDeckIO = (liftM infiniteShuffledDeck) . getStdGen
 
 
-testSourceUnbiased pvalue numSamples = undefined --getStdGen >>= getAdditionalDF
-        where getAdditionalDF gen = (snd $ genRange gen) - (fst $ genRange gen)
+--testSourceUnbiased pvalue numSamples = undefined --getStdGen >>= getAdditionalDF
+--        where getAdditionalDF gen = (snd $ genRange gen) - (fst $ genRange gen)
         -- ^ DOUBLE CHECK THIS
         
 
@@ -51,7 +48,7 @@ testSourceUnbiased pvalue numSamples = undefined --getStdGen >>= getAdditionalDF
 -- |Takes as input a set of bins with the number of observations in each
 -- returns the set of bins you would expect given an even distribution
 -- across all bins zipped with the observed values
-evenDistribution :: [Int] -> [(Int, Double)]
+evenDistribution :: U.Vector Int -> U.Vector (Int, Double)
 evenDistribution observed = zip observed expectedObservations
         where numBins = length observed
               totalObservations = fromIntegral . sum $ observed
