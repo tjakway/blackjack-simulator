df8174953705d6c8c68833604c6b377cd6e2c050
Thomas Jakway <tjakway@nyu.edu>
2016-04-19 07:23:20 -0400

    working on fuzzy checking options

--- test/Jakway/Blackjack/Fuzzy/Options.hs
+++ test/Jakway/Blackjack/Fuzzy/Options.hs
@@ -1,11 +1,47 @@
 module Jakway.Blackjack.Fuzzy.Options
+(
+Config(..)
+)
+where
 
 data Distribution = EvenDistribution | RNGDistribution
+
+data Config = Config
+            { distribution :: Distribution,
+              pvalue :: Double,
+              sampleSize :: Integer
+            }
 
 data Flag = WhichDistribution Distribution |
             PValue Double |
             SampleSize Integer
             deriving (Show, Eq)
+
+getWhichDistribution :: Flag -> Maybe Distribution
+getWhichDistribution EvenDistribution = Just EvenDistribution
+getWhichDistribution RNGDistribution = Just RNGDistribution
+getWhichDistribution _ = Nothing
+
+getPValue :: Flag -> Maybe Double
+getPValue (PValue val) = Just val
+getPValue _ = Nothing
+
+getSampleSize :: Flag -> Maybe Integer
+getSampleSize (SampleSize n) = Just n
+getSampleSize _ = Nothing
+
+flagsToConfig :: [Flag] -> Either String Config
+flagsToConfig [] = Left "No flags passed."
+flagsToConfig flags
+                | dist == Nothing = Left "No distribution passed."
+                | pval < 0 = Left "P value must be greater than 0."
+                | n < 104 = Left "Sample size must be greater than 104."
+                | otherwise = Right . Config $ fromJust <$> dist <*> pval <*> n
+        where dist = getSingleFlag getWhichDistribution flags
+              pval = getSingleFlag getPValue flags
+              n = getSingleFlag getSampleSize flags
+
+
 
 whichDistOpt, pvalueOpt, sampleSizeOpt :: String -> Flag
 whichDistOpt = WhichDistribution . read
@@ -19,3 +55,9 @@ options =
     , Option ['d']     ["distribution"]  (ReqArg  "distribution") "Which test to run (the expected distribution).  Possible values are EvenDistribution and RNGDistribution"
     ]
 
+
+--returns a valid configuration or prints an error and exits
+getConfig :: [String] -> IO Conf.Config
+getConfig argv = parseOptions argv options "Usage: blackjack-simulator [OPTION...]" >>= \(flags, _) -> return (flagsToConfig flags) >>=
+                                \res -> case res of (Left x) -> die $ "Error processing options: " ++ x
+                                                    (Right conf) -> return conf
