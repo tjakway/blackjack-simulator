5cf82021c7449b219d608ab691ea3208effd2e63
Thomas Jakway <tjakway@nyu.edu>
2016-04-19 10:28:19 -0400

    added RNG sample size option

--- test/Jakway/Blackjack/Random/Options.hs
+++ test/Jakway/Blackjack/Random/Options.hs
@@ -18,12 +18,14 @@ data Distribution = EvenDistribution |
 data Config = Config
             { distribution :: Distribution,
               pvalue :: Double,
-              sampleSize :: Integer
+              sampleSize :: Integer,
+              rngSampleSize :: Integer
             }
 
 data Flag = WhichDistribution Distribution |
             PValue Double |
-            SampleSize Integer
+            SampleSize Integer |
+            RNGSampleSize Integer
             deriving (Show, Eq)
 
 getWhichDistribution :: Flag -> Maybe Distribution
@@ -38,29 +40,37 @@ getSampleSize :: Flag -> Maybe Integer
 getSampleSize (SampleSize n) = Just n
 getSampleSize _ = Nothing
 
+getRNGSampleSize :: Flag -> Maybe Integer
+getRNGSampleSize (RNGSampleSize n) = Just n
+getRNGSampleSize _ = Nothing
+
 flagsToConfig :: [Flag] -> Either String Config
 flagsToConfig [] = Left "No flags passed."
 flagsToConfig flags
                 | dist == Nothing = Left "No distribution passed."
                 | pval < 0 = Left "P value must be greater than 0."
                 | n <= 104 = Left "Sample size must be greater than 104."
-                | otherwise = Right  (Config (fromJust dist) pval n)
+                | ((== RNGDistribution) . fromJust $ dist) && (rngSampleSize == Nothing) = Left "Must specify RNG sample size."
+                | otherwise = Right  (Config (fromJust dist) pval n (fromJust rngSampleSize))
         where dist = getSingleFlag getWhichDistribution flags
               pval = fromJust $ getSingleFlag getPValue flags
               n = fromJust $ getSingleFlag getSampleSize flags
+              rngSampleSize = getSingleFlag getRNGSampleSize flags
 
 
 
-whichDistOpt, pvalueOpt, sampleSizeOpt :: String -> Flag
+whichDistOpt, pvalueOpt, sampleSizeOpt, rngSampleSizeOpt :: String -> Flag
 whichDistOpt = WhichDistribution . read
 pvalueOpt = PValue . read
 sampleSizeOpt = SampleSize . read
+rngSampleSizeOpt = RNGSampleSize . read
 
 options :: [OptDescr Flag]
 options =
     [ Option ['p']        ["pvalue"]  (ReqArg pvalueOpt "percent")  "P value."
     , Option ['n']        ["sample-size"]  (ReqArg sampleSizeOpt  "num")  "Number of samples."
     , Option ['d']     ["distribution"]  (ReqArg  whichDistOpt "distribution") "Which test to run (the expected distribution).  Possible values are EvenDistribution and RNGDistribution"
+    , Option ['r']        ["rng-sample-size"] (ReqArg rngSampleSizeOpt "num") "Number of samples to take from the random number generator (should be at least several times the range)."
     ]
 
 
